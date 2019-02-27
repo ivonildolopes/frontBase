307a3751828ff366030f5d8c30f767753f25f0c5
ivonildo-lopes <ivonildolopes.ti@gmail.com>
2019-02-27 14:58:58 -0300

    fazendo upload de arquivo

--- src/app/pdf/pdf-service.service.ts
+++ src/app/pdf/pdf-service.service.ts
@@ -1,19 +1,20 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Response } from "../api.response.model";
 import { API_JAVA } from "../app.api";
+import { ResponseContentType } from "@angular/http";
 
-@Injectable(
-)
+@Injectable()
 export class PdfServiceService {
   constructor(
-    private http: HttpClient
-  ) //,private notificationService:NotificationService
-  //,private loading: LoadingService
-  {}
+    private http: HttpClient //,private notificationService:NotificationService //,private loading: LoadingService
+  ) {}
 
   geraPdf(): Observable<Response> {
+    //let headers = new HttpHeaders({ 'Content-Type': 'application/JSON' });
     return this.http.get<Response>(`${API_JAVA}/cidades/all`);
   }
+
+
 }
