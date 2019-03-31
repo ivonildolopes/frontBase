c7b21a06cbbdb04978ec0af5dd8ecb569c269fd7
ivonildo-lopes <ivonildolopes.ti@gmail.com>
2019-03-31 01:36:09 -0300

    preparando a tela de cadastro do veiculo

--- src/app/administracao/administracao.module.ts
+++ src/app/administracao/administracao.module.ts
@@ -7,11 +7,13 @@ import { AdministracaoService } from './administracao.service';
 import { VeiculoCadastroComponent } from './veiculo/veiculo-cadastro/veiculo-cadastro.component';
 import { VeiculoConsultaComponent } from './veiculo/veiculo-consulta/veiculo-consulta.component';
 import { VeiculoService } from './veiculo/veiculo.service';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    AdministracaoRoutingModule
+    AdministracaoRoutingModule,
+    MaterialModule
   ],
   declarations: [VeiculoCadastroComponent, VeiculoConsultaComponent],
   providers: [AdministracaoService, VeiculoService]
--- src/app/administracao/veiculo/veiculo-cadastro/veiculo-cadastro.component.ts
+++ src/app/administracao/veiculo/veiculo-cadastro/veiculo-cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-veiculo-cadastro',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VeiculoCadastroComponent implements OnInit {
 
-  constructor() { }
+  veiculoForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.veiculoForm = this.formBuilder.group({
+
+      placa: this.formBuilder.control(''),
+      isVendido: this.formBuilder.control('true')
+    })
   }
 
 }
