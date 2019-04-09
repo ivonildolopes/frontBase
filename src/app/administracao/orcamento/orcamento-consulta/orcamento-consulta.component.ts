bffa1d98798d0e317fcf48a07ffd8fe50d1f9151
ivonildorodrigues <ivonildo.rodrigues@pge.ce.gov.br>
2019-04-09 18:00:16 -0300

    criando consulta de orcamento

--- src/app/administracao/orcamento/orcamento-consulta/orcamento-consulta.component.ts
+++ src/app/administracao/orcamento/orcamento-consulta/orcamento-consulta.component.ts
@@ -1,3 +1,6 @@
+import { Router, ActivatedRoute } from '@angular/router';
+import { OrcamentoService } from './../orcamento.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrcamentoConsultaComponent implements OnInit {
 
-  constructor() { }
+  orcamentoForm: FormGroup;
+
+  comboList: any;
+  listVeiculos: any[];
+  listClientes: any[];
+
+  movimentacoes: any[];
+
+  constructor(private formBuilder: FormBuilder,
+    private service: OrcamentoService
+    , private router: Router
+    , private route: ActivatedRoute
+    ) { }
 
   ngOnInit() {
+    this.inicializaForm();
+
+    this.service.populaListasConsulta().subscribe(res => {
+      this.comboList = res.data;
+      this.listVeiculos = this.comboList.veiculos;
+    });
+
+
+  }
+
+  inicializaForm() {
+    this.orcamentoForm = this.formBuilder.group({      
+      veiculo: this.formBuilder.control(''),
+      data: this.formBuilder.control(''),
+      valor: this.formBuilder.control(''),
+    });
+  }
+
+  consultar() {
+    const entradaSaida = this.orcamentoForm.getRawValue();
+
+    // this.service.consultaByParams(entradaSaida).subscribe( res => {      
+    //   this.movimentacoes = res.data;
+    // });
+  }
+
+  limpar() {
+    this.inicializaForm();
+    this.movimentacoes = [];
+  }
+
+  novo() {
+    this.router.navigate(['entradaSaida/cadastro']);
+  }
+
+  editar(event) {
+
+  }
+
+  remover(event) {
+
   }
 
 }
--- src/app/administracao/orcamento/orcamento.service.ts
+++ src/app/administracao/orcamento/orcamento.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { LoadingService } from '../../shared/loading';
+import { NotificationService } from '../../shared/notification/notification.service';
+import { API_JAVA } from 'src/app/app.api';
+import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/finally';
+import { Response } from 'src/app/api.response.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrcamentoService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private loading: LoadingService,
+    private notificationService: NotificationService
+  ) {}
+
+  populaListas() {
+    return this.http.get<Response>(`${API_JAVA}/entradaSaida/listas`);
+  }
+
+  populaListasConsulta() {
+    return this.http.get<Response>(`${API_JAVA}/entradaSaida/listas/consulta`);
+  }
+
+  salvar(orcamento) {
+    return this.http.post<Response>(`${API_JAVA}/orcamento`, orcamento)
+    .subscribe(res => this.notificationService.send(res));
+  }
+
+  consultaByParams(orcamento) {
+    this.loading.display(true);
+    let params = new HttpParams();
+
+    if (orcamento.veiculo) { params = params.set('idVeiculo' , orcamento.veiculo.id); }
+    if (orcamento.data) { params = params.set('data' , orcamento.data); }
+    if (orcamento.isPago) { params = params.set('isPago' , orcamento.isPago); }
+
+    return this.http.get<Response>(`${API_JAVA}/orcamento/params`, { params });
+  }
 }
