b7c17e2c201170fa22ae545f6f0619e5d93e267b
ivonildo-lopes <ivonildolopes.ti@gmail.com>
2019-07-19 20:40:54 -0300

    mudando environment homo e prod

--- src/environments/environment.homo.ts
+++ src/environments/environment.homo.ts
@@ -3,7 +3,15 @@ export const environment = {
 
   api: 'http://localhost:8080' ,
   base: 'http://localhost:8080',
-  envName: 'prod',
+  envName: 'homo',
+  firebase: {
+    apiKey: "AIzaSyC_rUN-J7Mf1w76_oU_aJVpE5x-E3t1PYU",
+    authDomain: "teste-8951e.firebaseapp.com",
+    databaseURL: "https://teste-8951e.firebaseio.com",
+    projectId: "teste-8951e",
+    storageBucket: "teste-8951e.appspot.com",
+    messagingSenderId: "875279681287"
+  }
 };
 
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,14 @@ export const environment = {
   api: 'http://localhost:8080' ,
   base: 'http://localhost:8080',
   envName: 'prod',
+  firebase: {
+    apiKey: "AIzaSyC_rUN-J7Mf1w76_oU_aJVpE5x-E3t1PYU",
+    authDomain: "teste-8951e.firebaseapp.com",
+    databaseURL: "https://teste-8951e.firebaseio.com",
+    projectId: "teste-8951e",
+    storageBucket: "teste-8951e.appspot.com",
+    messagingSenderId: "875279681287"
+  }
 };
 
 
